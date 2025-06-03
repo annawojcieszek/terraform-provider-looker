c5f544ac61ab82f58c99b9c849113e4e77e24cf6
Anna Wojcieszek <wojcieszek.ania@gmail.com>
2025-06-03 15:40:19 +0200

    Fix go format

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -88,7 +88,7 @@ func New(version string) func() *schema.Provider {
 				"looker_user_attribute":         resourceUserAttribute(),
 				"looker_user_attribute_member":  resourceUserAttributeMember(),
 				"looker_theme":                  resourceTheme(),
-				"looker_api_credentials":         resourceApiCredentials(),
+				"looker_api_credentials":        resourceApiCredentials(),
 			},
 		}
 
